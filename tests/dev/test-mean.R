489abad7818842d6a9f78fa3138b5bc3d5a4aede
Abdullah <aoh123@gmail.com>
2020-04-20 23:51:17 -0400

    Update test-mean.R

--- tests/dev/test-mean.R
+++ tests/dev/test-mean.R
@@ -1,16 +1,23 @@
 rm(list = ls())
 library(ospsuite)
-devtools::load_all(".")
-tree <- getSimulationTree("./tests/dev/individualPksimSim.pkml")
+
+workflowFolder = paste0("C:/Users/ahamadeh/Dropbox/rproject/workflow/ex_",format(Sys.Date(), "%Y%m%d"), "_", format(Sys.time(), "%H%M%S"))
+
+devtools::load_all("C:/Users/ahamadeh/Dropbox/GitHub/OSP/OSPSuite.ReportingEngine")
+tree <- getSimulationTree("C:/Users/ahamadeh/Dropbox/GitHub/OSP/OSPSuite.ReportingEngine/tests/dev/individualPksimSim.pkml")
   ms <- SimulationSet$new(
   simulationSetName = "meansim",
-  simulationFile = "./tests/dev/individualPksimSim.pkml",
+  simulationFile = "C:/Users/ahamadeh/Dropbox/GitHub/OSP/OSPSuite.ReportingEngine/tests/dev/individualPksimSim.pkml",
   pathID = tree$Organism$Heart$Interstitial$smarties$Concentration$path
 )
 
-mwf <- MeanModelWorkflow$new(simulationSets = list(ms) , workflowFolder = "./ex")
+mwf <- MeanModelWorkflow$new(simulationSets = list(ms) , workflowFolder = workflowFolder)
+setwd(workflowFolder)
+
+
+
 mwf$simulate$settings$showProgress <- TRUE
-mwf$meanModelPKParameters$inactivate()
+mwf$meanModelPKParameters$activate()
 mwf$meanModelSensitivityAnalysis$inactivate()
 #mwf$meanModelSensitivityAnalysis$settings$variableParameterPaths <- c(tree$Organism$Heart$Volume$path)
 mwf$runWorkflow()
