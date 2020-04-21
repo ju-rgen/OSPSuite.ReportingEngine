1ab9e0b23292e712a0e596397aa1fe1bfd90ead2
Abdullah <aoh123@gmail.com>
2020-04-20 23:51:12 -0400

    Update simulation-task.R

--- R/simulation-task.R
+++ R/simulation-task.R
@@ -46,7 +46,6 @@ SimulationTask <- R6::R6Class(
     #' @param structureSets list of `SimulationStructure` R6 class
     runTask = function(structureSets) {
       re.tStartAction(actionType = "Simulation", re.className = class(self)[1], re.methodName = tail(strsplit(x = as.character(match.call()[1]), split = "$", fixed = TRUE)[[1]], 1))
-      print(as.character(match.call()))
       logWorkflow(
         message = paste0("Starting ", self$message),
         pathFolder = self$workflowFolder
